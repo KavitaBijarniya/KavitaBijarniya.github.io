a07b27621fef6811792af169330f178a6a80e860
KavitaBijarniya <kbijarniya922@gmail.com>
2026-03-24 10:07:22 +0530

    minor change

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/KavitaBijarniya.github.io/',  // 👈 ADD THIS LINE
+  base: '/kavitabijarniya.github.io/',  // 👈 ADD THIS LINE
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
